4369c8a8451b8e82c7db72b5d8232266f8ab3f23
ejberrio <everberrio.col@gmail.com>
2022-04-17 13:40:54 -0500

    Validations_models

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # Python
-from ast import Return
-import imp
 from typing import Optional
-from unittest import result
+from enum import Enum
 
 # Pydantic
 from pydantic import BaseModel
+from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI
@@ -18,6 +17,14 @@ app = FastAPI()
 # Models
 
 
+class HairColor(Enum):
+    white = "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+    red = "red"
+
+
 class Location(BaseModel):
     city: str
     state: str
@@ -25,11 +32,27 @@ class Location(BaseModel):
 
 
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
-    is_married: Optional[bool] = None
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115
+    )
+    hair_color: Optional[HairColor] = Field(
+        default=None
+    )
+    is_married: Optional[bool] = Field(
+        default=None
+    )
 
 
 @app.get("/")
@@ -40,11 +63,13 @@ def home():
 # Request and response body
 
 @app.post("/person/new")
-def create_person(person: Person = Body(...)):
+def create_person(
+    person: Person = Body(...)
+):
     return person
 
-
 # Validaciones: Query Parameters
+
 
 @app.get("/person/detail")
 def show_person(
